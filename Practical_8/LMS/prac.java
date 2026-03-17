108eed54018d9a451df26f6180bcf2e07582ce3d
aryan9272 <aryankale9272@gmail.com>
2026-03-18 00:48:27 +0530

    Add files via upload

---
+++ Practical_8/LMS/prac.java
@@ -0,0 +1,9 @@
+class prac{
+    public static void main(String[] args){
+        String ans="UTSAV";
+        for(int i=1;i<=2026;i++){
+            ans+="Utsav_"+i;
+        }
+        System.out.println(ans);
+    }
+}
