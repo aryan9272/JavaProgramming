e3fe6d18796f017dedc0907f3b4101167ce922a6
Aryan Kale <aryankale9272@gmail.com>
2026-04-24 00:35:37 +0530

    ""

--- Practical_8/LMS/prac.java
+++
@@ -1,9 +0,0 @@
-class prac{
-    public static void main(String[] args){
-        String ans="UTSAV";
-        for(int i=1;i<=2026;i++){
-            ans+="Utsav_"+i;
-        }
-        System.out.println(ans);
-    }
-}
